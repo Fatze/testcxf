39ca2db930ca887815d7186d8ef28325e83fb684
fatze <Fatze@tut.by>
2015-09-30 16:46:42 +0300

    Initial commit

---
+++ src/main/java/com/agalets/testcxf/ITestCXF.java
@@ -0,0 +1,8 @@
+package com.agalets.testcxf;
+
+import javax.jws.WebService;
+
+@WebService(name="testcxf")
+public interface ITestCXF {
+	String getName();
+}
---
+++ src/main/java/com/agalets/testcxf/TestCXF.java
@@ -0,0 +1,9 @@
+package com.agalets.testcxf;
+
+public class TestCXF implements ITestCXF {
+
+	public String getName() {
+		return "hello world";
+	}
+
+}
